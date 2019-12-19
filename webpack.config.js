78842ec58e5ef6e400df9a6fac6a3d9f0191ffde
Kevin Layfield <kevinjlayfield@gmail.com>
2019-12-19 09:08:45 -0500

    Use serverless-webpack to make webpack compile lambda functions on a serverless deploy command

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
 var path = require('path');
+const slsw = require('serverless-webpack');
 
 module.exports = {
     // Change to your "entry-point".
-    entry: './src/handlers/handler.ts',
+    entry: slsw.lib.entries,
+    //entry: './src/handlers/handler.ts',
     target: 'node',
     mode: 'development',
     output: {
-        path: path.resolve(__dirname, 'lib'),
-        filename: 'handler.js',
-        library: 'handler',
+        path: path.resolve(__dirname, 'webpack'),
+        filename: '[name].js',
         libraryTarget: 'commonjs2'
     },
     resolve: {
