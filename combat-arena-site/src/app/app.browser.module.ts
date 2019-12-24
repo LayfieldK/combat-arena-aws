842bcd5f9eb67c4eed9986f8a33a3a7a4d46b6e3
Kevin Layfield <kevinjlayfield@gmail.com>
2019-12-24 17:09:44 -0500

    Remove unused components

--- combat-arena-site/src/app/app.browser.module.ts
+++ combat-arena-site/src/app/app.browser.module.ts
@@ -2,16 +2,12 @@ import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-bro
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FirstComponent } from './first/first.component';
-import { SecondComponent } from './second/second.component';
-import { MenuComponent } from './menu/menu.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AppModule } from './app.module';
 
 @NgModule({
 imports: [
-  
   AppRoutingModule,
   HttpClientModule,
   AppModule,
--- combat-arena-site/src/app/app.module.ts
+++ combat-arena-site/src/app/app.module.ts
@@ -3,9 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FirstComponent } from './first/first.component';
-import { SecondComponent } from './second/second.component';
-import { MenuComponent } from './menu/menu.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
@@ -19,9 +16,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 @NgModule({
 declarations: [
   AppComponent,
-  FirstComponent,
-  SecondComponent,
-  MenuComponent,
   CombatantsComponent,
   CombatantDetailComponent,
   MessagesComponent,
