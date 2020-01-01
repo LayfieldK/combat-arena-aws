fb890e5351454fb3ec4660c7e64e533ff18646ed
Kevin Layfield <kevinjlayfield@gmail.com>
2020-01-01 13:41:33 -0500

    Add battles to in memory data service

--- combat-arena-site/src/app/in-memory-data.service.ts
+++ combat-arena-site/src/app/in-memory-data.service.ts
@@ -19,7 +19,21 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 19, name: 'Magma' },
       { id: 20, name: 'Tornado' }
     ];
-    return {combatants};
+    const battles = [
+      {
+        id: 1, name: 'Dr Nice vs Tornado',
+        combatants: [{ id: 11, name: 'Dr Nice' }, { id: 20, name: 'Tornado' }]
+      },
+      {
+        id: 1, name: 'Bombasto vs Magma',
+        combatants: [{ id: 13, name: 'Bombasto' }, { id: 19, name: 'Magma' }]
+      },
+      {
+        id: 1, name: 'Narco and Magneta vs Dynama and RubberMan',
+        combatants: [{ id: 12, name: 'Narco' }, { id: 15, name: 'Magneta' }, { id: 17, name: 'Dynama' }, { id: 16, name: 'RubberMan' }]
+      }
+    ];
+    return {combatants, battles};
   }
 
   // Overrides the genId method to ensure that a combatant always has an id.
