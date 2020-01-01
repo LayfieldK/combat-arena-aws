d357fa28a2ee4a08666b750e5981880ef31d0650
Kevin Layfield <kevinjlayfield@gmail.com>
2020-01-01 13:42:06 -0500

    Populate list of previous battles with the battles service

--- combat-arena-site/src/app/battles/battles.component.ts
+++ combat-arena-site/src/app/battles/battles.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Battle } from '../types/battle';
+import { BattleService } from '../battle.service';
 
 @Component({
   selector: 'app-battles',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BattlesComponent implements OnInit {
 
-  constructor() { }
+  battles: Battle[];
+  selectedBattle: Battle;
+
+  constructor(private battleService: BattleService) { }
 
   ngOnInit() {
+    this.getBattles();
+  }
+
+  getBattles(): void {
+    this.battleService.getBattles()
+      .subscribe(battles => this.battles = battles);
+  }
+
+  listClick(newValue: Battle) {
+    this.selectedBattle = newValue;
   }
 
 }
