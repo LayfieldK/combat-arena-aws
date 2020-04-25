0018ad4fab0c55f69fa4b22c058e22660c2ff4bc
Kevin Layfield <kevinjlayfield@gmail.com>
2020-04-24 22:02:18 -0400

    Fix some vulnerabilities and include angular material

--- combat-arena-site/src/app/app.browser.module.ts
+++ combat-arena-site/src/app/app.browser.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AppModule } from './app.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
 imports: [
   AppRoutingModule,
   HttpClientModule,
   AppModule,
-  BrowserTransferStateModule
+  BrowserTransferStateModule,
+  BrowserAnimationsModule
 ],
 providers: [],
 bootstrap: [AppComponent]
